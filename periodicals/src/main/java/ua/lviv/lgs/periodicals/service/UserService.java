655dfdef377c49baeab246f56a5dcf6b848caab6
Eugene <saenko_e@ukr.net>
2021-03-19 19:10:45 +0200

    added loggers to Service methods

--- periodicals/src/main/java/ua/lviv/lgs/periodicals/service/UserService.java
+++ periodicals/src/main/java/ua/lviv/lgs/periodicals/service/UserService.java
@@ -1,5 +1,7 @@
 package ua.lviv.lgs.periodicals.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,9 @@ import ua.lviv.lgs.periodicals.domain.UserRole;
 
 @Service
 public class UserService{
+	
+	private Logger logger = LoggerFactory.getLogger(UserService.class);
+	
     @Autowired
     private UserRepository userRepository;
 
@@ -18,6 +23,7 @@ public class UserService{
 
 
     public void save(User user) {
+    	logger.info("Register new user {}: " + user);
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         user.setPasswordConfirm(bCryptPasswordEncoder.encode(user.getPasswordConfirm()));
         user.setRole(UserRole.ROLE_USER);
@@ -25,6 +31,7 @@ public class UserService{
     }
     
     public User findByEmail(String email) {
+    	logger.info("Get user {} by email: " + email);
     	return userRepository.findByEmail(email).get();
     }
 
